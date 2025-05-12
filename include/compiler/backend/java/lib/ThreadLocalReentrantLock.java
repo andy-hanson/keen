bcdbe60ec5ac928b5194efa07eaadaf66c4b3a50
Andy <andy-hanson@protonmail.com>
2025-05-11 20:42:58 -0400

    wip

--- include/compiler/backend/java/lib/ThreadLocalReentrantLock.java
+++
@@ -1,7 +0,0 @@
-class ThreadLocalReentrantLock extends ThreadLocal<java.util.concurrent.locks.ReentrantLock> {
-	@Override protected java.util.concurrent.locks.ReentrantLock initialValue() {
-		java.util.concurrent.locks.ReentrantLock res = new java.util.concurrent.locks.ReentrantLock();
-		res.lock();
-		return res;
-	}
-}
